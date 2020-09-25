32847b2a36ad12c970c74f45be26003d152f0954
Sam Keathley <samncollette@gmail.com>
2020-09-25 16:24:41 -0700

    added loss condition and play again button functionality

---
+++ hangman/script.js
@@ -0,0 +1,113 @@
+const wordEl = document.getElementById('word');
+const wrongLettersEl = document.getElementById('wrong-letters');
+const playAgainBtn = document.getElementById('play-button');
+const popup = document.getElementById('popup-container');
+const notification = document.getElementById('notification-container');
+const finalMessage = document.getElementById('final-message');
+
+const figureParts = document.querySelectorAll('.figure-part');
+
+const words = ['application', 'programming', 'interface', 'wizard'];
+
+let selectedWord = words[Math.floor(Math.random() * words.length)];
+
+const correctLetters = [];
+const wrongLetters = [];
+
+function displayWord() {
+  wordEl.innerHTML = `
+  ${selectedWord
+    .split('')
+    .map(
+      (letter) => `
+    <span class="letter">${correctLetters.includes(letter) ? letter : ''}</span>
+    `
+    )
+    .join('')}
+  `;
+
+  const innerWord = wordEl.innerText.replace(/\n/g, '');
+
+  if (innerWord === selectedWord) {
+    finalMessage.innerText = 'You saved the man!';
+    popup.style.display = 'flex';
+  }
+}
+
+// Update wrong letters
+function updateWrongLettersEl() {
+  // Display wrong letters
+  wrongLettersEl.innerHTML = `
+  ${wrongLetters.length > 0 ? '<p>Wrong</p>' : ''}
+  ${wrongLetters.map((letter) => `<span>${letter}</span>`)}
+  `;
+
+  // Display parts
+  figureParts.forEach((part, index) => {
+    const errors = wrongLetters.length;
+
+    if (index < errors) {
+      part.style.display = 'block';
+    } else {
+      part.style.display = 'none';
+    }
+  });
+
+  // Lost condition
+  if (wrongLetters.length === figureParts.length) {
+    finalMessage.innerText =
+      "Unfortunately, I am the High King of Skyrim. This man's life is forfeit.";
+    popup.style.display = 'flex';
+    selectedWord = words[Math.floor(Math.random() * words.length)];
+  }
+}
+
+function showNotification() {
+  notification.classList.add('show');
+
+  setTimeout(() => {
+    notification.classList.remove('show');
+  }, 2000);
+}
+
+// Keydown letter press
+window.addEventListener('keydown', (e) => {
+  if (e.code >= 'KeyA' && e.code <= 'KeyZ') {
+    const letter = e.key;
+
+    if (selectedWord.includes(letter)) {
+      if (!correctLetters.includes(letter)) {
+        correctLetters.push(letter);
+
+        displayWord();
+      } else {
+        showNotification();
+      }
+    } else {
+      if (!wrongLetters.includes(letter)) {
+        wrongLetters.push(letter);
+
+        updateWrongLettersEl();
+      } else {
+        showNotification();
+      }
+    }
+  }
+});
+
+// Play again
+playAgainBtn.addEventListener('click', () => {
+  // Empty correct and wrong arrays
+  correctLetters.splice(0);
+  wrongLetters.splice(0);
+
+  selectedWord = words[Math.floor(Math.random() * words.length)];
+
+  displayWord();
+
+  updateWrongLettersEl();
+
+  popup.style.display = 'none';
+});
+
+displayWord();

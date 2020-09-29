3719390406142b9d37bfa61c06e1cb0e64f19c8d
Sam Keathley <samncollette@gmail.com>
2020-09-29 12:51:01 -0700

    add infinite scroll and loading animation

--- infinite-scroll/script.js
+++ infinite-scroll/script.js
@@ -2,7 +2,7 @@ const postContainer = document.getElementById('post-container');
 const loading = document.querySelector('.loader');
 const filter = document.getElementById('filter');
 
-let limit = 3;
+let limit = 5;
 let page = 1;
 
 async function getPosts() {
@@ -34,4 +34,26 @@ async function showPosts() {
   });
 }
 
+// Show loader and fetch more posts
+function showLoading() {
+  loading.classList.add('show');
+
+  setTimeout(() => {
+    loading.classList.remove('show');
+
+    setTimeout(() => {
+      page++;
+      showPosts();
+    }, 300);
+  }, 1000);
+}
+
 showPosts();
+
+window.addEventListener('scroll', () => {
+  const { scrollTop, scrollHeight, clientHeight } = document.documentElement;
+
+  if (scrollTop + clientHeight >= scrollHeight - 5) {
+    showLoading();
+  }
+});

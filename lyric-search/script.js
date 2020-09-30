8dc1ae9fe428a15167ae390cbbb4e52604662205
Sam Keathley <samncollette@gmail.com>
2020-09-30 16:16:10 -0700

    get and display song lyrics

--- lyric-search/script.js
+++ lyric-search/script.js
@@ -55,6 +55,20 @@ async function getMoreSongs(url) {
   showData(data);
 }
 
+async function getLyrics(artist, songTitle) {
+  const res = await fetch(`${apiURL}/v1/${artist}/${songTitle}`);
+  const data = await res.json();
+
+  const lyrics = data.lyrics.replace(/(\r\n|\r|\n)/g, '<br>');
+
+  result.innerHTML = `
+    <h2><strong>${artist}</strong> - ${songTitle}</h2>
+    <span>${lyrics}</span>
+  `;
+
+  more.innerHTML = '';
+}
+
 // Event Listeners
 form.addEventListener('submit', (e) => {
   e.preventDefault();
@@ -65,5 +79,16 @@ form.addEventListener('submit', (e) => {
     alert('Please type in a search term');
   } else {
     searchSongs(searchTerm);
+  }
+});
+
+result.addEventListener('click', (e) => {
+  const clickedEl = e.target;
+
+  if (clickedEl.tagName === 'BUTTON') {
+    const artist = clickedEl.getAttribute('data-artist');
+    const songTitle = clickedEl.getAttribute('data-songtitle');
+
+    getLyrics(artist, songTitle);
   }
 });

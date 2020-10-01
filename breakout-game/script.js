c335c542983ea52968f63370cea8697c7a5c2a47
Sam Keathley <samncollette@gmail.com>
2020-10-01 11:16:12 -0700

    draw bricks

--- breakout-game/script.js
+++ breakout-game/script.js
@@ -6,6 +6,9 @@ const ctx = canvas.getContext('2d');
 
 let score = 0;
 
+const brickRowCount = 9;
+const brickColumnCount = 5;
+
 const ball = {
   x: canvas.width / 2,
   y: canvas.height / 2,
@@ -15,7 +18,6 @@ const ball = {
   dy: -4,
 };
 
-// Create Paddle Propr
 const paddle = {
   x: canvas.width / 2 - 40,
   y: canvas.height - 20,
@@ -25,7 +27,26 @@ const paddle = {
   dx: 0,
 };
 
-// Draw ball on canvas
+const brickInfo = {
+  w: 70,
+  h: 20,
+  padding: 10,
+  offsetX: 45,
+  offsetY: 60,
+  visible: true,
+};
+
+const bricks = [];
+for (let i = 0; i < brickRowCount; i++) {
+  bricks[i] = [];
+  for (let j = 0; j < brickColumnCount; j++) {
+    const x = i * (brickInfo.w + brickInfo.padding) + brickInfo.offsetX;
+    const y = j * (brickInfo.h + brickInfo.padding) + brickInfo.offsetY;
+    bricks[i][j] = { x, y, ...brickInfo };
+  }
+}
+console.log(bricks);
+
 function drawBall() {
   ctx.beginPath();
   ctx.arc(ball.x, ball.y, ball.size, 0, Math.PI * 2);
@@ -34,7 +55,6 @@ function drawBall() {
   ctx.closePath();
 }
 
-// Draw paddle on canvas
 function drawPaddle() {
   ctx.beginPath();
   ctx.rect(paddle.x, paddle.y, paddle.w, paddle.h);
@@ -43,16 +63,28 @@ function drawPaddle() {
   ctx.closePath();
 }
 
+function drawScore() {
+  ctx.font = '20px Cambria';
+  ctx.fillText(`Score: ${score}`, canvas.width - 100, 30);
+}
+
+function drawBricks() {
+  bricks.forEach((column) => {
+    column.forEach((brick) => {
+      ctx.beginPath();
+      ctx.rect(brick.x, brick.y, brick.w, brick.h);
+      ctx.fillStyle = brick.visible ? '#0095dd' : 'transparent';
+      ctx.fill();
+      ctx.closePath();
+    });
+  });
+}
 // Draw everything
 function draw() {
   drawBall();
   drawPaddle();
   drawScore();
-}
-
-function drawScore() {
-  ctx.font = '20px Cambria';
-  ctx.fillText(`Score: ${score}`, canvas.width - 100, 30);
+  drawBricks();
 }
 
 draw();

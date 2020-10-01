f2ccdeb13910f9a154e699f0fb65428066788b4f
Sam Keathley <samncollette@gmail.com>
2020-10-01 13:41:26 -0700

    scramble list items

--- list-sort/script.js
+++ list-sort/script.js
@@ -21,12 +21,16 @@ let dragStartIndex;
 createList();
 
 function createList() {
-  [...richestPeople].forEach((person, index) => {
-    const listItem = document.createElement('li');
+  [...richestPeople]
+    .map((a) => ({ value: a, sort: Math.random() }))
+    .sort((a, b) => a.sort - b.sort)
+    .map((a) => a.value)
+    .forEach((person, index) => {
+      const listItem = document.createElement('li');
 
-    listItem.setAttribute('data-index', index);
+      listItem.setAttribute('data-index', index);
 
-    listItem.innerHTML = `
+      listItem.innerHTML = `
       <span class="number">${index + 1}</span>
       <div class="draggable" draggable="true">
         <p class="person-name">${person}</p>
@@ -34,8 +38,8 @@ function createList() {
       </div>
     `;
 
-    listItems.push(listItem);
+      listItems.push(listItem);
 
-    draggable_list.appendChild(listItem);
-  });
+      draggable_list.appendChild(listItem);
+    });
 }

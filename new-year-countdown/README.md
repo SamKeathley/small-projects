ccbfb135e5f7aa0a4f49c4031dc42e3a14fb419e
Sam Keathley <samncollette@gmail.com>
2020-10-01 13:13:03 -0700

    dynamic year disaply and loading spinner

--- new-year-countdown/README.md
+++ new-year-countdown/README.md
@@ -1,3 +1,5 @@
 # New Year Countdown
 
 A simple landing page with a countdown from current date to the next new year.
+
+**Photo By** [Luke Jones](https://unsplash.com/@impossible_monster?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText) on [Unsplash](https://unsplash.com/s/photos/new-year?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText)
--- new-year-countdown/script.js
+++ new-year-countdown/script.js
@@ -3,10 +3,14 @@ const hours = document.getElementById('hours');
 const minutes = document.getElementById('minutes');
 const seconds = document.getElementById('seconds');
 const countdown = document.getElementById('countdown');
+const year = document.getElementById('year');
+const loading = document.getElementById('loading');
 
 const currentYear = new Date().getFullYear();
 
 const newYearTime = new Date(`January 01 ${currentYear + 1} 00:00:00`);
+
+year.innerText = currentYear + 1;
 
 function updateCountdown() {
   const currentTime = new Date();
@@ -22,5 +26,10 @@ function updateCountdown() {
   minutes.innerHTML = m < 10 ? '0' + m : m;
   seconds.innerHTML = h < 10 ? '0' + s : s;
 }
+
+setTimeout(() => {
+  loading.remove();
+  countdown.style.display = 'flex';
+}, 1000);
 
 setInterval(updateCountdown, 1000);

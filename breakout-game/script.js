9419a592be22c20f2205e29f709abd12c96131c9
Sam Keathley <samncollette@gmail.com>
2020-10-01 11:31:04 -0700

    move paddle and add wall detection

--- breakout-game/script.js
+++ breakout-game/script.js
@@ -79,17 +79,64 @@ function drawBricks() {
     });
   });
 }
+
+function movePaddle() {
+  paddle.x += paddle.dx;
+
+  // wall detection
+  if (paddle.x + paddle.w > canvas.width) {
+    paddle.x = canvas.width - paddle.w;
+  }
+
+  if (paddle.x < 0) {
+    paddle.x = 0;
+  }
+}
+
 // Draw everything
 function draw() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+
   drawBall();
   drawPaddle();
   drawScore();
   drawBricks();
 }
 
-draw();
+function update() {
+  movePaddle();
+
+  draw();
+
+  requestAnimationFrame(update);
+}
+
+update();
+
+function keyDown(e) {
+  if (e.key === 'Right' || e.key === 'ArrowRight') {
+    paddle.dx = paddle.speed;
+  } else if (e.key === 'Left' || e.key === 'ArrowLeft') {
+    paddle.dx = -paddle.speed;
+  }
+}
+
+function keyUp(e) {
+  if (
+    e.key === 'Right' ||
+    e.key === 'ArrowRight' ||
+    e.key === 'Left' ||
+    e.key === 'ArrowLeft'
+  ) {
+    paddle.dx = 0;
+  }
+}
 
 // Event Listeners
+
+document.addEventListener('keydown', keyDown);
+document.addEventListener('keyup', keyUp);
+
 rulesBtn.addEventListener('click', () => {
   rules.classList.add('show');
 });

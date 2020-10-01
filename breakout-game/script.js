124fd04c23019704190dc49686d592d77b8c9545
Sam Keathley <samncollette@gmail.com>
2020-10-01 12:28:38 -0700

    move ball and break bricks

--- breakout-game/script.js
+++ breakout-game/script.js
@@ -93,6 +93,47 @@ function movePaddle() {
   }
 }
 
+function moveBall() {
+  ball.x += ball.dx;
+  ball.y += ball.dy;
+
+  // Wall collision (x || right/left)
+  if (ball.x + ball.size > canvas.width || ball.x - ball.size < 0) {
+    ball.dx *= -1; // ball.dx = ball.dx * -1
+  }
+
+  // Wall collision (y || top/bottom)
+  if (ball.y + ball.size > canvas.height || ball.y - ball.size < 0) {
+    ball.dy *= -1;
+  }
+
+  // Paddle collision
+  if (
+    ball.x - ball.size > paddle.x &&
+    ball.x + ball.size < paddle.x + paddle.w &&
+    ball.y + ball.size > paddle.y
+  ) {
+    ball.dy = -ball.speed;
+  }
+
+  // Brick collision
+  bricks.forEach((column) => {
+    column.forEach((brick) => {
+      if (brick.visible) {
+        if (
+          ball.x - ball.size > brick.x && // left brick side check
+          ball.x + ball.size < brick.x + brick.w && // right brick side check
+          ball.y + ball.size > brick.y && // top brick side check
+          ball.y - ball.size < brick.y + brick.h // bottom brick side check
+        ) {
+          ball.dy *= -1;
+          brick.visible = false;
+        }
+      }
+    });
+  });
+}
+
 // Draw everything
 function draw() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
@@ -105,6 +146,7 @@ function draw() {
 
 function update() {
   movePaddle();
+  moveBall();
 
   draw();
 

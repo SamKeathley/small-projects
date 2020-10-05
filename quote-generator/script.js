a622d0dd8306021ccbf71c54c7dbd5b57ce0be34
Sam Keathley <samncollette@gmail.com>
2020-10-05 13:04:52 -0700

    add loader

--- quote-generator/script.js
+++ quote-generator/script.js
@@ -3,8 +3,22 @@ const quoteText = document.getElementById('quote');
 const authorText = document.getElementById('author');
 const twitterBtn = document.getElementById('twitter');
 const quoteBtn = document.getElementById('new-quote');
+const loader = document.getElementById('loader');
+
+function loading() {
+  loader.hidden = false;
+  quoteContainer.hidden = true;
+}
+
+function complete() {
+  if (!loader.hidden) {
+    quoteContainer.hidden = false;
+    loader.hidden = true;
+  }
+}
 
 async function getQuote() {
+  loading();
   const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
   const apiUrl = `http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json`;
   try {
@@ -22,8 +36,9 @@ async function getQuote() {
       quoteText.classList.remove('long-text');
     }
     quoteText.innerText = data.quoteText;
+    complete();
   } catch (error) {
-    console.log('Oof, no quote', error);
+    getQuote();
   }
 }
 

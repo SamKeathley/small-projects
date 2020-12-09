379cbbd464839b65604edb8b142832e57a52f458
Sam Keathley <samncollette@gmail.com>
2020-12-09 12:10:22 -0800

    update big cup with amount remaining

--- water-reminder/script.js
+++ water-reminder/script.js
@@ -3,6 +3,8 @@ const liters = document.getElementById('liters')
 const percentage = document.getElementById('percentage')
 const remaining = document.getElementById('remaining')
 
+updateBigCup()
+
 smallCups.forEach((cup, index) => {
   cup.addEventListener('click', () => highlightCups(index))
 })
@@ -22,4 +24,28 @@ function highlightCups(index) {
       cup.classList.remove('full')
     }
   })
+
+  updateBigCup()
+}
+
+function updateBigCup() {
+  const fullCups = document.querySelectorAll('.cup-small.full').length
+  const totalCups = smallCups.length
+
+  if (fullCups === 0) {
+    percentage.style.visibility = 'hidden'
+    percentage.style.height = 0
+  } else {
+    percentage.style.visibility = 'visible'
+    percentage.style.height = `${(fullCups / totalCups) * 330}px`
+    percentage.innerText = `${(fullCups / totalCups) * 100}%`
+  }
+
+  if (fullCups === totalCups) {
+    remaining.style.visibility = 'hidden'
+    remaining.style.height = 0
+  } else {
+    remaining.style.visibility = 'visible'
+    liters.innerText = `${2 - (250 * fullCups) / 1000}L`
+  }
 }

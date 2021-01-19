6524d034081909ab8b602c1cfa4f88f043aa27d8
Sam Keathley <samncollette@gmail.com>
2021-01-19 14:05:41 -0800

    populate current time and duration of video

--- video-player-2/script.js
+++ video-player-2/script.js
@@ -31,9 +31,19 @@ video.addEventListener('ended', showPlayIcon);
 
 // Progress Bar ---------------------------------- //
 
+// Calculate display time format
+let displayTime = (time) => {
+  const minutes = Math.floor(time / 60);
+  let seconds = Math.floor(time % 60);
+  seconds = seconds > 9 ? seconds : `0${seconds}`;
+  return `${minutes}:${seconds}`;
+};
+
 // Update progress bar as video plays
 let updateProgress = () => {
   progressBar.style.width = `${(video.currentTime / video.duration) * 100}%`;
+  currentTime.textContent = `${displayTime(video.currentTime)} /`;
+  duration.textContent = `${displayTime(video.duration)}`;
 };
 
 // Volume Controls --------------------------- //

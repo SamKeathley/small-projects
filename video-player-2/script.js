c0cd932ccfacf3bc45307c8f39ce44901b4a4c04
Sam Keathley <samncollette@gmail.com>
2021-01-19 13:51:09 -0800

    play and pause video functionality

--- video-player-2/script.js
+++ video-player-2/script.js
@@ -1,21 +1,42 @@
-
+const video = document.querySelector('video');
+const progressRange = document.querySelector('.progress-range');
+const progressBar = document.querySelector('.progress-bar');
+const playBtn = document.getElementById('play-btn');
+const volumeIcon = document.getElementById('volume-icon');
+const volumeRange = document.querySelector('.volume-range');
+const volumeBar = document.querySelector('.volume-bar');
+const currentTime = document.querySelector('.time-elapsed');
+const duration = document.querySelector('.time-duration');
+const fullscreenBtn = document.querySelector('.fullscreen');
 
 // Play & Pause ----------------------------------- //
+let showPlayIcon = () => {
+  playBtn.classList.replace('fa-pause', 'fa-play');
+  playBtn.setAttribute('title', 'Play');
+};
 
+let togglePlay = () => {
+  if (video.paused) {
+    video.play();
+    playBtn.classList.replace('fa-play', 'fa-pause');
+    playBtn.setAttribute('title', 'Pause');
+  } else {
+    video.pause();
+    showPlayIcon();
+  }
+};
 
+// On video end, show play button icon
+video.addEventListener('ended', showPlayIcon);
 
 // Progress Bar ---------------------------------- //
 
-
-
 // Volume Controls --------------------------- //
-
-
 
 // Change Playback Speed -------------------- //
 
-
-
 // Fullscreen ------------------------------- //
 
-
+// Event Listeners
+playBtn.addEventListener('click', togglePlay);
+video.addEventListener('click', togglePlay);

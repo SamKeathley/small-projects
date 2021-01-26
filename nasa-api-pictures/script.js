545dde3765c47f93f6612d7982aa1d843f5de2b4
Sam Keathley <samncollette@gmail.com>
2021-01-26 14:04:17 -0800

    fetch from NASA API

---
+++ nasa-api-pictures/script.js
@@ -0,0 +1,20 @@
+// NASA API
+const count = 10;
+const apiKey = 'DEMO_KEY';
+const apiUrl = `https://api.nasa.gov/planetary/apod?api_key=${apiKey}&count=${count}`;
+
+let resultsArray = [];
+
+// Get 10 images from API
+const getNasaPictures = async () => {
+  try {
+    const response = await fetch(apiUrl);
+    resultsArray = await response.json();
+    console.log(resultsArray);
+  } catch (error) {
+    // Catch error here
+  }
+};
+
+// On load
+getNasaPictures();

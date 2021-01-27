0b999c94f05c5c4eadb14b1d4580ca42fbe5e11f
Sam Keathley <samncollette@gmail.com>
2021-01-27 13:21:18 -0800

    radio selection and functionality

---
+++ maths-game/script.js
@@ -0,0 +1,59 @@
+const gamePage = document.getElementById('game-page');
+const scorePage = document.getElementById('score-page');
+const splashPage = document.getElementById('splash-page');
+const countdownPage = document.getElementById('countdown-page');
+
+const startForm = document.getElementById('start-form');
+const radioContainers = document.querySelectorAll('.radio-container');
+const radioInputs = document.querySelectorAll('input');
+const bestScores = document.querySelectorAll('.best-score-value');
+
+const countdown = document.querySelector('.countdown');
+
+const itemContainer = document.querySelector('.item-container');
+
+const finalTimeEl = document.querySelector('.final-time');
+const baseTimeEl = document.querySelector('.base-time');
+const penaltyTimeEl = document.querySelector('.penalty-time');
+const playAgainBtn = document.querySelector('.play-again');
+
+// Equations
+let questionAmount = 0;
+let equationsArray = [];
+
+// Game Page
+let firstNumber = 0;
+let secondNumber = 0;
+let equationObject = {};
+const wrongFormat = [];
+
+// get value from selected radio btn
+const getRadioValue = () => {
+  let radioValue;
+  radioInputs.forEach((radioInput) => {
+    if (radioInput.checked) {
+      radioValue = radioInput.value;
+    }
+  });
+  return radioValue;
+};
+
+// Form that desides amt of qs
+const selectQuestionAmount = (e) => {
+  e.preventDefault();
+  questionAmount = getRadioValue();
+  console.log('Question Amount:', questionAmount);
+};
+
+startForm.addEventListener('click', () => {
+  radioContainers.forEach((radioEl) => {
+    // Remove 'selected label' styling
+    radioEl.classList.remove('selected-label');
+    // Add it back if radio input is checked
+    if (radioEl.children[1].checked) {
+      radioEl.classList.add('selected-label');
+    }
+  });
+});
+
+startForm.addEventListener('submit', selectQuestionAmount);

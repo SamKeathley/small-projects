23d0830f6aed2149b6a2441b6682eba854166225
Sam Keathley <samncollette@gmail.com>
2021-02-03 16:17:44 -0800

    copy generated password to clipboard

--- password-generator/script.js
+++ password-generator/script.js
@@ -14,6 +14,22 @@ const randomFunc = {
   symbol: getRandomSymbol
 };
 
+clipboardEl.addEventListener('click', () => {
+  const textarea = document.createElement('textarea');
+  const password = resultEl.innerText;
+
+  if (!password) {
+    return;
+  }
+
+  textarea.value = password;
+  document.body.appendChild(textarea);
+  textarea.select();
+  document.execCommand('copy');
+  textarea.remove();
+  alert('Coppied to Clipboard!');
+});
+
 generateEl.addEventListener('click', () => {
   const length = +lengthEl.value;
   const hasLower = lowercaseEl.checked;

e40b93d1f92a242eea2fb61f9b4a1667b302d244
Sam Keathley <samncollette@gmail.com>
2021-02-08 13:32:20 -0800

    create dynamic cards

--- pokedex/script.js
+++ pokedex/script.js
@@ -1,21 +1,27 @@
 const poke_container = document.getElementById('poke-container');
 const pokemon_count = 150;
 const colors = {
-  fire: '#FDDFDF',
-  grass: '#DEFDE0',
-  electric: '#FCF7DE',
-  water: '#DEF3FD',
-  ground: '#f4e7da',
-  rock: '#d5d5d4',
-  fairy: '#fceaff',
-  poison: '#98d7a5',
-  bug: '#f8d5a3',
-  dragon: '#97b3e6',
-  psychic: '#eaeda1',
-  flying: '#F5F5F5',
-  fighting: '#E6E0D4',
-  normal: '#F5F5F5'
+  normal: '#A8A77A',
+  fire: '#EE8130',
+  water: '#6390F0',
+  electric: '#F7D02C',
+  grass: '#7AC74C',
+  ice: '#96D9D6',
+  fighting: '#C22E28',
+  poison: '#A33EA1',
+  ground: '#E2BF65',
+  flying: '#A98FF3',
+  psychic: '#F95587',
+  bug: '#A6B91A',
+  rock: '#B6A136',
+  ghost: '#735797',
+  dragon: '#6F35FC',
+  dark: '#705746',
+  steel: '#B7B7CE',
+  fairy: '#D685AD'
 };
+
+const main_types = Object.keys(colors);
 
 const fetchPokemon = async () => {
   for (let i = 1; i <= pokemon_count; i++) {
@@ -27,6 +33,39 @@ const getPokemon = async (id) => {
   const url = `https://pokeapi.co/api/v2/pokemon/${id}`;
   const res = await fetch(url);
   const data = await res.json();
+  createPokemonCard(data);
+};
+
+const createPokemonCard = (pokemon) => {
+  const pokemonEl = document.createElement('div');
+  pokemonEl.classList.add('pokemon');
+
+  const name = pokemon.name[0].toUpperCase() + pokemon.name.slice(1);
+  const id = pokemon.id.toString().padStart(3, '0');
+
+  const poke_types = pokemon.types.map((type) => type.type.name);
+  const type = main_types.find((type) => poke_types.indexOf(type) > -1);
+  const color = colors[type];
+
+  pokemonEl.style.backgroundColor = color;
+
+  const pokemonInnerHTML = `
+  <div class="img-container">
+    <img
+     src="https://pokeres.bastionbot.org/images/pokemon/${pokemon.id}.png"
+     alt=""
+   />
+  </div>
+  <div class="info">
+    <span class="number">#${id}</span>
+    <h3 class="name">${name}</h3>
+    <small class="type"><span class="first-type">${type}</span></small>
+  </div>
+  `;
+
+  pokemonEl.innerHTML = pokemonInnerHTML;
+
+  poke_container.appendChild(pokemonEl);
 };
 
 fetchPokemon();
